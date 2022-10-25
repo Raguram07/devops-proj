ea55102d8a608c4e5b8252e203d1cab17f775ea8
Raguram07 <raguram0702@gmail.com>
2022-10-25 19:28:16 +0530

    dockerfile added - app data included

--- main.tf
+++ main.tf
@@ -1,8 +1,24 @@
 provider "aws" {
   region = "us-east-1"
+  access_key=AWS_ACCESS_KEY_ID
+  secret_key=AWS_SECRET_ACCESS_KEY
 }
 
-resource "aws_s3_bucket" "terraform_state" {
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-devopsthehardway"
+    key    = "ecr-terraform.tfstate"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
   bucket = "terraform-state-devopsthehardway"
   versioning {
     enabled = true
@@ -15,4 +31,132 @@ resource "aws_s3_bucket" "terraform_state" {
       }
     }
   }
+}
+
+
+resource "aws_ecr_repository" "devopsthehardway-ecr-repo" {
+  name                 = var.repo_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-devopsthehardway"
+    key    = "eks-terraform-workernodes.tfstate"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+
+# IAM Role for EKS to have access to the appropriate resources
+resource "aws_iam_role" "eks-iam-role" {
+  name = "devopsthehardway-eks-iam-role"
+
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+
+}
+
+## Attach the IAM policy to the IAM role
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-iam-role.name
+}
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly-EKS" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks-iam-role.name
+}
+
+## Create the EKS cluster
+resource "aws_eks_cluster" "devopsthehardway-eks" {
+  name = "devopsthehardway-cluster"
+  role_arn = aws_iam_role.eks-iam-role.arn
+
+  vpc_config {
+    subnet_ids = [var.subnet_id_1, var.subnet_id_2]
+  }
+
+  depends_on = [
+    aws_iam_role.eks-iam-role,
+  ]
+}
+
+## Worker Nodes
+resource "aws_iam_role" "workernodes" {
+  name = "eks-node-group-example"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.workernodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.workernodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "EC2InstanceProfileForImageBuilderECRContainerBuilds" {
+  policy_arn = "arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds"
+  role       = aws_iam_role.workernodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.workernodes.name
+}
+
+resource "aws_eks_node_group" "worker-node-group" {
+  cluster_name    = aws_eks_cluster.devopsthehardway-eks.name
+  node_group_name = "devopsthehardway-workernodes"
+  node_role_arn   = aws_iam_role.workernodes.arn
+  subnet_ids      = [var.subnet_id_1, var.subnet_id_2]
+  instance_types = ["t3.xlarge"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    #aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
 }
